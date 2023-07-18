f162738d88f039cd36aec5973ab96773fc2d7501
MirkoGLostia <giggi.torre@mail.com>
2023-07-18 12:53:31 +0200

    model

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -10,14 +10,14 @@ class Comic extends Model
     use HasFactory;
 
     protected $fillable = [
+
         "title",
         "description",
         "thumb",
         "price",
         "series",
         "sale_date",
-        "type",
-        "artists",
-        "writers"
+        "type"
+        
     ];
 }
